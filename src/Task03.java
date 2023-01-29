f4ac4375d0d4a1f1938b9e55bbbb776b1138cdf3
Kate Belozertseva <katebelozertseva@gmail.com>
2023-01-29 14:08:30 +0300

    Log for calculator

--- src/Task03.java
+++ src/Task03.java
@@ -1,9 +1,24 @@
 /*Реализовать простой калькулятор (пользователь вводит 2 числа и вводит операцию (+ - / *). int a ;
 int b; String op (op!=”Stop”); (char != ’b’)
+Добавить систему логирования!
  */
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.*;
 
 public class Task03 {
+    static void  Log(String text) throws IOException {
+        Logger logger = Logger.getLogger(Task03.class.getName());
+        FileHandler file = new FileHandler("log.log", true);
+        logger.addHandler(file);
+        SimpleFormatter form = new SimpleFormatter();
+        file.setFormatter(form);
+        logger.setUseParentHandlers(false); //чтобы не выводил логи в консоль
+        logger.info(text);
+        file.close();
+    }
+
+
     static int Calculator(String a, String b){
         String[] numbers = a.split(" ");
         int numA = Integer.parseInt(numbers[0]);
@@ -15,23 +30,30 @@ public class Task03 {
         else return -1;
     }
 
-    static void Endless(){
+    static void Endless() throws IOException {
         boolean stop = true;
         while (stop) {
             Scanner num = new Scanner(System.in);
             System.out.println("Введите два числа через пробел или stop для выхода: ");
             String math = num.nextLine();
-            if (math.equals("stop")) break; //Подскажите, как сделать без break? Он вообще не воспринимает изменнеие булевой переменной в теле цикла
+            Log(math);
+            if (math.equals("stop")) {
+                num.close();
+                break;
+            }
 //            System.out.println(math);
             Scanner plus = new Scanner(System.in);
             System.out.println("Введите знак (+, -, / или *), чтобы произвести математическое действие");
             String sign = plus.next();
+            Log(sign);
             int res = Calculator(math, sign);
+            String result = "" + res;
+            Log(result);
             System.out.println(res);
         }
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         Endless();
     }
 }
