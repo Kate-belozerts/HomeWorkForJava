79f6602dd2e1d118e9753318a3b674fb924ac38f
Kate Belozertseva <katebelozertseva@gmail.com>
2023-01-29 14:09:40 +0300

    Corrected loop

--- src/Task02.java
+++ src/Task02.java
@@ -1,14 +1,11 @@
 /*Вывести все простые числа от 1 до 1000*/
 import java.util.Scanner;
 public class Task02 {
-    static void SimpleNumbers(int number) { // Объясните, пожалуйста, почему цифра 4 проходит?
+    static void SimpleNumbers(int number) {
         if (number == 0 || number < 0) System.out.println("Это не является натуральным числом");
         System.out.print("2 ");
         int num = 3;
-//        while (num < number){ // И почему в данном случае цикл while не работает? Улетает в бесконечность.
-//        Если num становится больше number - это же условие выхода из цикла...
-            for (int i = 2; i < num; i++) {
-                if (num >= number) break; // Пришлось добавить эту строчку вместо while((
+            for (int i = 2; i < number; i++) {
                 if (num % i == 0){
                     num++;
                     i = 2;
