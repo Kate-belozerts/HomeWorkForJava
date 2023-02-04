f3a96f97fc247636b138004dc64d69a61f1647e1
Kate Belozertseva <katebelozertseva@gmail.com>
2023-02-04 18:13:04 +0300

    Reverse of the LinkedList

--- src/Task12.java
+++ src/Task12.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Task12 {
+import java.util.LinkedList;
+import java.util.Stack;
+
+/*Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.*/
+public class Task12 {
+    static public void main(String [] args){
+        LinkedList<Integer> lst = new LinkedList<Integer>();
+        for (int i = 0; i < 7; i++) {
+            lst.add(i);
+        }
+        System.out.print(lst);
+        System.out.println();
+        lst = Reverse(lst);
+        System.out.print(lst);
+    }
+    static public LinkedList<Integer> Reverse(LinkedList<Integer> arr){
+        Stack<Integer> res = new Stack<Integer>();
+        for (int i = 0; i < arr.size(); i++) {
+            res.add(arr.get(i));
+        }
+        int size = res.size();
+        arr.clear();
+        for (int j = 0; j < size; j++) {
+            arr.add(j, res.pop());
+        }
+        return arr;
+    }
 }
