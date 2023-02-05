3d69f9285131a4c1dcf95eb7fe0ab7d70af07203
Kate Belozertseva <katebelozertseva@gmail.com>
2023-02-05 15:11:21 +0300

    Methods for LinkedList

--- src/Task13.java
+++ src/Task13.java
@@ -1,32 +1,92 @@
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.Queue;
 
 /*Реализуйте очередь с помощью LinkedList со следующими методами: enqueue() - помещает элемент в конец очереди,
 dequeue() - возвращает первый элемент из очереди и удаляет его, first() - возвращает первый элемент из очереди,
 не удаляя.
  */
 public class Task13 {
+
     public static void main(String[] args) {
-        Queue<Integer> lst = new LinkedList<Integer>();
-//        System.out.print(lst);
-        enqueue(lst, 3);
-        enqueue(lst, 7);
-        enqueue(lst, 5);
-        enqueue(lst, 9);
-        System.out.print(lst);
-        dequeue(lst);
-        System.out.print(lst);
-        System.out.println();
-        int res = first(lst);
-        System.out.print(res);
+        Queue arr = new Queue(5); // Реализовала очередь по праивлам FIFO
+        arr.enqueue(1);
+        arr.enqueue(2);
+        arr.enqueue(3);
+        arr.enqueue(4);
+        arr.dequeue();
+        arr.first();
+
+        //С помощью встроенных методов:
+        LinkedList<Integer> lst = new LinkedList<>();
+        Eenqueue(lst, 5);
+        Eenqueue(lst, 4);
+        Eenqueue(lst, 3);
+        System.out.println(lst);
+        Ddequeue(lst);
+        System.out.println(lst);
+        int res = Ffirst(lst);
+        System.out.println(res);
+        System.out.println("The element is still in the list: " + lst);
     }
-    public static void enqueue(Queue<Integer> elements, int position){
-        elements.add(position);
+
+
+    static void Eenqueue(LinkedList<Integer> ls, int element){
+        ls.add(element);
     }
-    public static void dequeue(Queue<Integer> items){
-        items.remove();
+    static void Ddequeue(LinkedList<Integer> ls){
+        ls.remove();
     }
-    public static int first(Queue<Integer> items){
-        return items.element();
+    static int Ffirst(LinkedList<Integer> ls){
+        return ls.peek();
+    }
+
+
+
+    // Методы для массива:_________________________________________________________________________
+static class Queue{
+        int capacity;
+        int[] items;
+        int last;
+        int back;
+        Queue(int size){
+            items = new int[size];
+            last = size -1;
+            back = 0;
+            capacity = size;
+        }
+        boolean isFull(){
+            if (back == last){ //return back == last;
+                return true;
+            }
+            else return false;
+        }
+        public void enqueue(int element){
+            if (isFull()){
+                System.out.println("Queue is full");
+            }
+            items[back] = element;
+            System.out.println(Arrays.toString(items));
+            back++;
+        }
+        void dequeue() {
+            if (back == 0) System.out.println("Queue is empty");
+            else {
+                System.out.println("Deleted: " + items[0]);
+                for (int i = 1; i < back; i++) {
+                    items[i-1] = items[i];
+                    if(i == back-1) items[i] = 0;
+                }
+
+            }
+            System.out.println(Arrays.toString(items));
+        }
+
+        void first(){
+            if (back == 0) System.out.println("Queue is empty");
+            else {
+                System.out.println("First element is: " + items[0]);
+            }
+        }
     }
 }
+
